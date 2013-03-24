a146c00bae36483db0523d96cd6039c7674cb450
erindrummond <e.b.drummond@gmail.com>
2013-03-24 10:26:03 -0600

    cust sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 StoreEngine::Application.routes.draw do
+  get "sessions/new"
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
