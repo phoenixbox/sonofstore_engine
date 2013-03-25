4f9f5986fc45cdf63a352ae287798aec7019f6cb
erindrummond <e.b.drummond@gmail.com>
2013-03-25 14:52:47 -0600

    controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,6 +7,53 @@ describe SessionsController do
       get 'new'
       response.should be_success
     end
+
+    it 'renders the new template'
+  end
+
+  describe 'POST #create' do
+
+    shared_examples "a user that fails to login" do
+      it "asks the user to login again" do
+        post :create, email: "breaking@bad.com"
+        expect(response).to render_template :new
+      end
+
+      it "does not set up the user session" do
+        post :create, email: "breaking@bad.com"
+        expect(session[:user_id]).to be_nil
+      end
+    end
+
+    context "no user matches email address" do
+      it_behaves_like "a user that fails to login"
+    end
+
+    context "user with email address" do
+
+      let!(:user) { User.create first_name: "Walter", last_name: "White", email: "breaking@bad.com", password: "meth" }
+
+      context "when the user provides an incorrect password" do
+        it_behaves_like "a user that fails to login"
+      end
+
+      context "when the user provides a correct password" do
+        it "redirects to user to the root url" do
+          post :create, email: "breaking@bad.com", password: "meth"
+          expect(response).to redirect_to root_path
+        end
+
+        it "sets up the user session" do
+          post :create, email: "breaking@bad.com", password: "meth"
+          expect(session[:user_id]).to eq user.id
+        end
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it "clears the session user id"
+    it "redirects to the root url"
   end
 
 end
