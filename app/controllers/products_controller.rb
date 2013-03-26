c9cc0b5aa827c7bdba67df1df5df4893232531fe
erindrummond <e.b.drummond@gmail.com>
2013-03-26 13:07:30 -0600

    testing controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,37 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    if @product.save
+      redirect_to product_path(@product), notice: "Product Added!"
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update_attributes params[:product]
+      #flash[:message] = "You're awesome, you updated the product"
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
 end
