3af305d1b1433abfc8e3a847c239876c591ec8da
Paul <pnblackwell@gmail.com>
2013-03-31 18:14:40 -0600

    working on login/signup w/ simple form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
   helper_method :current_user
 
   def authorize
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,14 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now.alert = "Email or password is invalid"
+      flash[:error] = "Wrong user email and/or password"
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out!"
+    flash[:notice] = "You successfully logged out!"
+    redirect_to root_url
   end
 end
