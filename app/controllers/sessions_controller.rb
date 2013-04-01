6be7dfe64612f08c19280fcec74f8dd054bcd480
Kareem Grant <kareem@getuserwise.com>
2013-04-01 13:17:02 -0600

    removing font-awesome manual installation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
