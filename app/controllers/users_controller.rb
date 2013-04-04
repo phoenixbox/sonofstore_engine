62fe956d9f8efc5fb54c51888c0bfa1eb9509f22
Kareem Grant <kareem@getuserwise.com>
2013-04-03 21:28:32 -0600

    added user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,32 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Thank you for signing up!"
     else
       render "new"
+    end
+  end
 
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:user])
+      redirect_to @user, notice: 'User information was successfully updated'
+    else
+      flash[:error] = "There was a problem saving your record"
+      render :edit
     end
   end
 end
