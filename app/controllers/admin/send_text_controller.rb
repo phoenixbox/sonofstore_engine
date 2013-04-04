fdedc4af3649ab40bd6151d0ff33a3d0c83a4944
Kareem Grant <kareem@getuserwise.com>
2013-04-04 13:05:58 -0600

    updated charge.js

--- app/controllers/admin/send_text_controller.rb
+++ app/controllers/admin/send_text_controller.rb
@@ -1,21 +1,21 @@
-# class Admin::SendTextController < Admin::BaseController
-#   def index
-#   end
+class Admin::SendTextController < Admin::BaseController
+  def index
+  end
 
-#   def send_text_message
-#     number_to_send_to = params[:number_to_send_to]
+  def send_text_message
+    number_to_send_to = params[:number_to_send_to]
 
-#     twilio_sid = "#"
-#     twilio_token = "#"
-#     twilio_phone_number = "12402930574"
+    twilio_sid = ENV["TWILIO_SID"]
+    twilio_token = EVN["TWILIO_TOKEN"]
+    twilio_phone_number = "12402930574"
 
-#     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
-#     @twilio_client.account.sms.messages.create(
-#       :from => "+1#{twilio_phone_number}",
-#       :to => number_to_send_to,
-#       :body => "Your order has been shipped! and will be,
-#         delivered on Monday, April 7th, 2013"
-#     )
-#   end
-# end
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => "Your order has been shipped! and will be,
+        delivered on Monday, April 7th, 2013"
+    )
+  end
+end
