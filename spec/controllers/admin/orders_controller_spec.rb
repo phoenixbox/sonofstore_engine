46d6e40e3e13e1d97f52d4e93e6d95adf458dfb4
Paul <pnblackwell@gmail.com>
2013-04-03 20:22:28 -0600

    pushing for view review

--- spec/controllers/admin/orders_controller_spec.rb
+++ spec/controllers/admin/orders_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Admin::OrdersController do
 
   # describe "GET 'show,'" do
+    
   #   it "returns http success" do
   #     get 'show,'
   #     response.should be_success
