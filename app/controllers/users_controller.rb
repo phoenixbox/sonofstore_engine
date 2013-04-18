969217082d513bea48d65c2eb14eb95947c01ccd
Kyle Suss <susskyle@gmail.com>
2013-04-18 04:33:39 -0600

    profile styles and homepage changes'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   layout 'session'
+  layout 'profile', only: [:show]
 
   def index
     @users = User.all
@@ -30,6 +31,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(current_user.id)
+    # render :layout => 'profile'
   end
 
   def edit
