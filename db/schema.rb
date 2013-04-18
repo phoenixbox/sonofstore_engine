6c516eabf6f27905ec5ff3a12b28554e32bda0ea
Kyle Suss <susskyle@gmail.com>
2013-04-17 21:10:42 -0600

    styling admin dash

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416234353) do
+ActiveRecord::Schema.define(:version => 20130417032558) do
 
   create_table "billing_addresses", :force => true do |t|
     t.integer  "consumer_id"
